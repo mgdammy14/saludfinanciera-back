a21f43d3c9bef0016ed46e71ec723684a85a1497
Gonzalo Pingo <mgdammy14@gmail.co>
2022-02-07 23:38:35 -0500

    desarrollo

--- CRUD-Factura/Startup.cs
+++ CRUD-Factura/Startup.cs
@@ -83,7 +83,7 @@ namespace CRUD_Factura
             services.AddScoped<IPersonLogic, PersonLogic>();
 
             services.AddSingleton<IUnitOfWork>(option => new UnitOfWork(
-                    Configuration.GetConnectionString("production-azure")
+                    Configuration.GetConnectionString("develop-azure")
                 ));
 
             services.AddAuthorization(auth => {
